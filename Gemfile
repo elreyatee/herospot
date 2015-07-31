bfd95a58f645517d27601205c9aa93b51e79d9ef
Ellery Temple Jr <elreyatee@gmail.com>
2015-07-31 05:26:04 -0700

    added sessions controller spec

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ gem 'bcrypt'
 group :development, :test do
   gem 'pry'
   gem 'pry-nav'
-  gem 'rspec-rails', '2.99'
+  gem 'rspec-rails'
   gem 'fabrication'
   gem 'faker'
   gem 'capybara'
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,12 +1,58 @@
 require 'spec_helper'
 
-describe SessionsController do  
+describe SessionsController do 
+  let(:aaron) { Fabricate(:user) } 
 
   describe "GET new" do 
+    it "redirects to front page if logged in" do 
+      session[:user_id] = aaron.id
+      get :new
+      expect(response).to redirect_to root_path
+    end
 
+    it "renders new template for unauthenticated users" do 
+      get :new
+      expect(response).to render_template :new
+    end
   end
 
   describe "POST create" do
+    context "with valid credentials" do 
+      before do 
+        post :create, full_name: aaron.full_name, email: aaron.email, password: aaron.password
+      end
+
+      it "sets the user in session" do 
+        session[:user_id] = aaron.id
+        expect(session[:user_id]).not_to be_nil
+      end
+
+      it "set the notice" do 
+        expect(flash[:notice]).not_to be_blank
+      end
+
+      it "redirects user to front page" do 
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "with invalid credentials" do 
+      before do 
+        post :create, full_name: aaron.full_name, email: aaron.email, password: "oops"
+      end
+
+      it "does not put user in the session" do 
+        expect(session[:user_id]).to be_nil
+      end
+
+      it "sets the error message" do 
+        expect(flash[:error]).not_to be_blank
+      end
+
+      it "renders login page" do 
+        expect(response).to render_template :new
+      end
+    end
 
   end
 
