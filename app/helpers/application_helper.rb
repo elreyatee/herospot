5b2c86989f61240567b19b336fd84c2e9f1752d3
Ellery Temple Jr <elreyatee@gmail.com>
2015-08-09 08:02:19 -0700

    updated index.html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,10 @@
 module ApplicationHelper
-  # def collect_ids
-  #   Hero.pluck(:id)
-  # end
-
   def display_heros(n = 1)
     result = []
     until result.size == n do
       ids = Hero.pluck(:id).sample
       hero = Hero.find(ids)
-      result << hero unless result.include?(hero)
+      result << hero if !result.include?(hero)
     end
     result
   end  
