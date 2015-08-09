c7b0e038545770c387252e65c4a6b3dec5c9d306
Ellery Temple Jr <elreyatee@gmail.com>
2015-08-09 07:46:09 -0700

    fixed display heros method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,14 @@
 module ApplicationHelper
-  def collect_ids
-    Hero.pluck(:id)
-  end
+  # def collect_ids
+  #   Hero.pluck(:id)
+  # end
 
   def display_heros(n = 1)
     result = []
     until result.size == n do
-      hero = Hero.find(collect_ids.sample)
+      # ids = Hero.pluck(:id).sample
+      # hero = Hero.find(collect_ids.sample)
+      hero = Hero.find(ids = Hero.pluck(:id).sample)
       result << hero unless result.include?(hero)
     end
     result
